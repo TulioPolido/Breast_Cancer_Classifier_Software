ea30a57a4df9967f93fcdb03e47e38b4bee87fc6
Túlio Polido <tuliopolido@gmail.com>
2020-10-10 17:48:42 -0300

    Código refatorado para melhor entendimento

--- main_dev.py
+++ main_dev.py
@@ -6,17 +6,10 @@ import os
 import pydicom
 import cv2
 import matplotlib.pyplot as plt
-'''try:
-    from Tkinter import * 
-    import tkFileDialog as filedialog
-except ImportError:
-    from tkinter import * 
-    from tkinter import filedialog
-'''
-
 
 class App(Frame):
     filename = ''
+
     def convert_to_png(self, file):
         """Utiliza matplotlib e pydicom para converter .dcm para .png"""
         filename = file
@@ -29,6 +22,7 @@ class App(Frame):
         plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
         plt.show()
         print(dataset.pixel_array)
+    ################### FIM convert_to_png ###################
 
     def chg_image(self):
         """Atualiza a imagem na tela"""
@@ -37,10 +31,12 @@ class App(Frame):
         else:
             self.img =  ImageTk.PhotoImage(self.im)
         self.la.config(image=self.img, bg="#000000", width=self.img.width(), height=self.img.height())
+    ################### FIM chg_image ###################
 
     def open(self):
         """Exibe opção para o usuário selecionar a imagem"""
         self.filename = filedialog.askopenfilename()
+        self.temImagemLabel = True
         if self.filename != "":
             if self.filename.endswith('.png') or self.filename.endswith('.tiff'): #Se a imagem for DICOM necessita de conversão
                 self.im =  Image.open(self.filename)
@@ -49,17 +45,11 @@ class App(Frame):
                 self.convert_to_png(self.filename)
 
         #self.chg_image()
-
-    def del_img(self):
-        if self.filename != '':
-            self.filename = ''
-            self.la.config(image='',bg="#FFFFFF",width=5,height=5)
-        else:
-            self.popupmsg(title="ATENÇÃO",msg="Não é possível apagar a imagem", geometry="300x80")
+    ################### FIM open ###################
 
     def zoom_in(self):
         """Faz zoom in na imagem exibida"""
-        if self.filename != '':
+        if self.temImagemLabel:
             w = self.im.width
             h = self.im.height
             if w < 800 or h < 800: #Limite superior de 800px na imagem
@@ -67,21 +57,27 @@ class App(Frame):
                 h = int(h * 1.1)
             self.im = self.im.resize((w,h))
             self.chg_image()
+        elif self.temImagemCanvas:
+            print('Zoom no canvas')
         else:
             self.popupmsg(title="ATENÇÃO",msg="Não é possível dar zoom", geometry="300x80")
+    ################### FIM zoom_in ###################
 
     def zoom_out(self):
         """Faz zoom out na imagem exibida"""
-        if self.filename != '':
+        if self.temImagemLabel:
             w = self.im.width
             h = self.im.height
             if w > 100 or h > 100: #Limite inferior de 100px na imagem
-                w = int(w * 0.9)
-                h = int(h * 0.9)
+                w = int(w * 0.9090)
+                h = int(h * 0.9090)
             self.im = self.im.resize((w,h))
             self.chg_image()
+        elif self.temImagemCanvas:
+            print('Zoom no Canvas')
         else:
             self.popupmsg(title="ATENÇÃO",msg="Não é possível dar zoom", geometry="300x80")
+    ################### FIM zoom_out ###################
 
     def ler_dir(self):
         """Le o diretório e 4 subdiretórios para carregas as imagens para a memória"""
@@ -99,17 +95,22 @@ class App(Frame):
             self.popupmsg(title="ATENÇÃO",msg=str(len(self.imagens)) + " imagens carregadas com sucesso!", geometry="300x80")
         except:
             self.popupmsg(title="ATENÇÃO",msg="Erro ao carregar imagens. Verifique o diretório!", geometry="300x80")
+    ################### FIM ler_dir ###################
 
     def selec_car(self):
         self.popupmsg(title="ATENÇÃO",msg="Função não implementada!", geometry="300x80")
+    ################### FIM select_car ###################
 
     def trein_clas(self):
         self.popupmsg(title="ATENÇÃO",msg="Função não implementada!", geometry="300x80")
+    ################### FIM trein_clas ###################
 
-    def anal_area(self):
+    def analisar_area(self):
         self.popupmsg(title="ATENÇÃO",msg="Função não implementada!", geometry="300x80")
+    ################### FIM analisar_area ###################
 
     def popupmsg(self, title, msg, geometry):
+        """Posta uma imagem em popup para o usuário""" 
         popup = tk.Toplevel()
         popup.wm_title(title)
         popup.geometry(geometry)
@@ -118,8 +119,53 @@ class App(Frame):
         B1 = tk.Button(popup, text="OK", command = popup.destroy)
         B1.pack()
         popup.mainloop()
+    ################### FIM popupmsg ###################
 
     def select_area(self):
+        def get_mouse_posn(event):
+            """Pega a posição do mouse ao clique do usuário""" 
+            nonlocal topy, topx, botx, boty
+            nonlocal rect_id
+            topx, topy = event.x, event.y
+            botx = topx + 64
+            boty = topy + 64
+            topx = topx - 64
+            topy = topy - 64
+            self.canvas.coords(rect_id, topx, topy, botx, boty) 
+
+        def popup_request():
+            """Pede ao usuário o nome do arquivo a ser salvo""" 
+            cropfilename = ''
+            
+            def set_text(popup):
+                nonlocal cropfilename
+                cropfilename = str(text.get())
+                popup.destroy()
+                return
+
+            popup = tk.Toplevel()
+            popup.wm_title("Nome do arquivo")
+            popup.geometry("280x80")
+            text = StringVar()
+            labelInst = tk.Label(popup, padx=40, text="Digite um nome para o arquivo: ").grid(row=0, column=5)
+            labelText = tk.Entry(popup, textvariable=text).grid(row=1, column=5)
+            B1 = tk.Button(popup, text="Salvar", command =lambda:set_text(popup)).grid(row=4, column=5)
+            popup.mainloop()
+            return str(cropfilename)
+
+        def confirm_cut(event):
+            """Recorta a região de interesse do usuário"""  
+            nonlocal topx, topy, botx, boty
+            border = (topx, topy, botx, boty)
+            imgCrop = Image.open(self.filename).crop(border)
+            nameFile = popup_request()
+           
+            if (nameFile.endswith('.png') == False):   #Verificando extensão do arquivo
+                nameFile = nameFile + '.png' 
+            self.popupmsg(title="Seleção de Região",msg="Imagem " + nameFile + " salva com sucesso", geometry="300x80")
+            imgCrop.save(nameFile, "PNG")
+            return
+
         # Alerta ao usuario caso ainda não tenha aberto uma imagem
         if self.filename == '': 
             #tk.messagebox.showwarning(title="ATENÇÃO", message="Selecione uma imagem primeiro")
@@ -134,68 +180,27 @@ class App(Frame):
         topx, topy, botx, boty = 0, 0, 0, 0
         rect_id = None
 
-        def get_mouse_posn(event):
-            nonlocal topy, topx, botx, boty
-            nonlocal rect_id
-            topx, topy = event.x, event.y
-            botx = topx + 64
-            boty = topy + 64
-            topx = topx - 64
-            topy = topy - 64
-            canvas.coords(rect_id, topx, topy, botx, boty) 
-
         img = ImageTk.PhotoImage(Image.open(self.filename))
-        canvas = tk.Canvas(self.la, width=img.width(), height=img.height(),
-                        borderwidth=0, highlightthickness=0)
-        canvas.pack(expand=True)
-        canvas.img = img  
-        canvas.create_image(0, 0, image=img, anchor=tk.NW)
+        self.canvas = tk.Canvas(self.la, width=img.width(), height=img.height(), borderwidth=0, highlightthickness=0)
+        self.canvas.pack(expand=True)
+        self.canvas.img = img  
+        self.canvas.create_image(0, 0, image=img, anchor=tk.NW)
+        self.temImagemCanvas = True
 
-        # Desenha retangulo verde em cima da imagem
-        rect_id = canvas.create_rectangle(topx, topy, botx, boty,
-                                        fill='', outline='LimeGreen', width=2.0)
-
-        def popup_request():
-            cropfilename = ''
-            def set_text(popup):
-                nonlocal cropfilename
-                cropfilename = str(text.get())
-                popup.destroy()
-                return
-            popup = tk.Toplevel()
-            popup.wm_title("Nome do arquivo")
-            popup.geometry("280x80")
-            text = StringVar()
-            labelInst = tk.Label(popup, padx=40, text="Digite um nome para o arquivo: ").grid(row=0, column=5)
-            labelText = tk.Entry(popup, textvariable=text).grid(row=1, column=5)
-            B1 = tk.Button(popup, text="Salvar", command =lambda:set_text(popup)).grid(row=4, column=5)
-            popup.mainloop()
-            return str(cropfilename)
-
-        # Função para recortar região de interesse selecionada pelo usuário
-        def confirm_cut(event):  
-            nonlocal topx, topy, botx, boty
-            border = (topx, topy, botx, boty)
-            imgCrop = Image.open(self.filename).crop(border)
-            nameFile = popup_request()
-            ''' Verificando extensão do arquivo '''
-            if (nameFile.endswith('.png') == False):   
-                nameFile = nameFile + '.png' 
-            self.popupmsg(title="Seleção de Região",msg="Imagem " + nameFile + " salva com sucesso", geometry="300x80")
-            imgCrop.save(nameFile, "PNG")
-            return
+        rect_id = self.canvas.create_rectangle(topx, topy, botx, boty, fill='', outline='LimeGreen', width=2.0) # Desenha retangulo verde em cima da imagem
         
-        canvas.bind('<Button-1>', get_mouse_posn)
-        # O usuario deve dar clique duplo para confirmar corte
-        canvas.bind('<Double-Button-1>', confirm_cut) 
+        self.canvas.bind('<Button-1>', get_mouse_posn)
+        self.canvas.bind('<Double-Button-1>', confirm_cut) # O usuario deve dar clique duplo para confirmar corte
         self.la.config(image='',bg="#FFFFFF",width=5,height=5) #Remove a imagem atras do canvas
-        ############## Fim select area ##############
+        self.temImagemLabel = False
+    ################### FIM select_area ###################
                
-
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master.title('Trabalho de Processamento de Imagens')
         self.imagens = []
+        self.temImagemLabel = False
+        self.temImagemCanvas = False
 
         #Tela do software
         fram = Frame(self)
@@ -206,7 +211,7 @@ class App(Frame):
         Button(fram, text="Selecionar Características", command=self.selec_car).pack(side=LEFT)
         Button(fram, text="Treinar classificador", command=self.trein_clas).pack(side=LEFT)
         Button(fram, text="Selecionar Região",  command=self.select_area).pack(side=LEFT)
-        Button(fram, text="Analisar área selecionada", command=self.anal_area).pack(side=LEFT)
+        Button(fram, text="Analisar área selecionada", command=self.analisar_area).pack(side=LEFT)
         fram.pack(side=TOP, fill=BOTH)
 
         #Área em que a imagem ficará presente
