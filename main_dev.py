226ac5ab1588a5939ea8fd80a51a151eed4d8d0e
ArthurHRT <arthurhr.teixeira@gmail.com>
2020-11-21 17:02:45 -0300

    Correção no método print

--- main_dev.py
+++ main_dev.py
@@ -531,13 +531,13 @@ class App(Frame):
                 string += ('\nHomogeneidade:\t%.6f\n'%(carac[i]))
                 i+=1
             if self.caracteristicas[3]:
-                string += ('\nContraste:\t\t%.6f\n'%(carac[i]))
+                string += ('\nContraste:\t%.6f\n'%(carac[i]))
         if espec is not None:
             string += ('\nEspecificidade:\t%.6f\n'%(espec))
         if acc is not None:
             string += ('\nPrecisão:\t\t%.1f%%\n'%(acc*100))
         if matriz is not None:
-            string += ('\nMatriz de confusão:\n\n\t1\t2\t3\t4\n___________________________________________________\n1|\t%d\t%d\t%d\t%d\n2|\t%d\t%d\t%d\t%d\n3|\t%d\t%d\t%d\t%d\n 4|\t%d\t%d\t%d\t%d\n'\
+            string += ('\nMatriz de confusão:\n\n\t1\t2\t3\t4\n___________________________________________________\n1|\t%d\t%d\t%d\t%d\n2|\t%d\t%d\t%d\t%d\n3|\t%d\t%d\t%d\t%d\n4|\t%d\t%d\t%d\t%d\n'\
                         %(matriz[0][0],matriz[0][1],matriz[0][2],matriz[0][3],\
                         matriz[1][0],matriz[1][1],matriz[1][2],matriz[1][3],\
                         matriz[2][0],matriz[2][1],matriz[2][2],matriz[2][3],\
