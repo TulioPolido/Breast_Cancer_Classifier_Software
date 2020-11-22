e909e0fc1cdb942cf779d09c465919e41a83f893
Túlio Polido <tuliopolido@gmail.com>
2020-11-21 23:20:57 -0300

    Linha em brabco

--- main_dev.py
+++ main_dev.py
@@ -36,6 +36,7 @@ class App(Frame):
 
         result = []
         w = 1.00000000023
+        
         #adiciona os valores normalizados de Hu para uma lista
         result.append(-1*copysign(1.0, huMoments[0][0])*log10(abs(huMoments[0][0])+w))
         result.append(-1*copysign(1.0, huMoments[1][0])*log10(abs(huMoments[1][0])+w))
